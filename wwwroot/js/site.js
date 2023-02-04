fee24f8fdc6ae33a268c67d560038e526a84f0cc
Hoàng Nguyên Bách <88475555+Bachhoang2001@users.noreply.github.com>
2023-02-04 21:06:20 +0700

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+(function ($) {
+    'use strict';
+
+    var $accountDelete = $('#delete-account'),
+        $accountDeleteDialog = $('#confirm-delete'),
+        transition;
+
+    $accountDelete.on('click', function () {
+        $accountDeleteDialog[0].showModal();
+        transition = window.setTimeout(function () {
+            $accountDeleteDialog.addClass('dialog-scale');
+        }, 0.5);
+    });
+
+    $('#cancel').on('click', function () {
+        $accountDeleteDialog[0].close();
+        $accountDeleteDialog.removeClass('dialog-scale');
+        clearTimeout(transition);
+    });
+
+})(jQuery);
